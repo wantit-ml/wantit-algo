bd8c7649508834acd12700583e518b4eca21986f
DigitalJay <pushkareva_lisa@mail.ru>
2021-06-21 20:41:20 +0300

    Improved converter
    Should be used for users and also for HRs

--- baseline.py
+++ baseline.py
@@ -1,26 +1,19 @@
 from db_help import *
 
 class Converter():
-    def __init__(self):
-        self.baseline_techs=["C++",
-                "C#",
-                "Django",
-                "Docker"
-                "Figma",
-                "Git",
-                "Go",
-                "Java",
-                "JavaScript",
-                "Keras",
-                "Python",
-                "Pythorch",
-                "SQL",
-                "React"
-                ]
+    def __init__(self, baseline_techs: list, baseline_lanuages:list):
+        self.baseline_techs=baseline_techs
+        self.baseline_languages=baseline_lanuages
+
     def convert(self, data:About) -> int:
         stack_num=['0' for i in range(len(self.baseline_techs))]
         for tech_name in data['stack']:
             stack_num[self.baseline_techs.index(tech_name)]='1'
-        return int(''.join(stack_num), 2)
 
+        lang_num=['0' for i in range(len(self.baseline_languages))]
+        for lang_name in data['foreign_languages']:
+            lang_num[self.baseline_languages.index(lang_name)]='1'
+
+        return int(''.join(stack_num + lang_num), 2)
+    
 
