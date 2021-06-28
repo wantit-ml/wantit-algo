a5b0935d159d9d229ce7aa8605179dd258f74487
DigitalJay <pushkareva_lisa@mail.ru>
2021-06-28 09:52:59 +0300

    Improved vacanes recomendation

--- matchers_old_vers.py
+++ matchers_old_vers.py
@@ -2,6 +2,15 @@ from db_help import *
 from typing import Union, List
 from cossim import SimCosModel
 
+
+def count_ones(x):
+    result = 0
+    while x > 0:
+        result += x & 1
+        x = x >> 1
+    return result
+
+
 """ 
 should be called as 
  users_for_vacancy = await MatchForHR.search_users(vacancy, list_of_about_objects)
@@ -9,12 +18,12 @@ should be called as
 
 
 class MatchForHR():
-
-    cute_model=SimCosModel()
+    cute_model = SimCosModel()
 
     @classmethod
     async def search_users(cls, vacancy: Vacancy, users_list: List[About]) -> List[int]:
         matched_users_ids = []
+        skills_needed=vacancy.code.count("1")
         for candidate in users_list:
             vacancy_code_len = len(vacancy.code)
             candidate_code_len = len(candidate.code)
@@ -26,7 +35,7 @@ class MatchForHR():
             else:
                 vacancy_code <<= (candidate_code_len - vacancy_code_len)
 
-            if vacancy_code & candidate_code >= vacancy_code:
+            if count_ones(vacancy_code & candidate_code)/skills_needed >= 0.6:
                 matched_users_ids.append(candidate.id)
         return cls.cute_model.rang_candidates(vacancy, matched_users_ids)
 
@@ -38,23 +47,25 @@ should be called as
 
 
 class MatchForUser():
-
-    cute_model=SimCosModel()
+    cute_model = SimCosModel()
 
     @classmethod
     async def search_vacancies(cls, user_info: About, vacancies_list: List[Vacancy]) -> List[int]:
         matched_vacancies_ids = []
+
         for sample_vacancy in vacancies_list:
             sample_vacancy_code_len = len(sample_vacancy.code)
             user_info_code_len = len(user_info.code)
             sample_vacancy_code = int(sample_vacancy.code, 2)
             user_info_code = int(user_info.code, 2)
 
+            skills_needed=sample_vacancy.code.count("1")
+
             if sample_vacancy_code_len > user_info_code_len:
                 user_info_code <<= (sample_vacancy_code_len - user_info_code_len)
             else:
                 sample_vacancy_code <<= (user_info_code_len - sample_vacancy_code_len)
 
-            if sample_vacancy_code & user_info_code >= sample_vacancy_code:
+            if count_ones(sample_vacancy_code & user_info_code)/ skills_needed >= 0.6:
                 matched_vacancies_ids.append(sample_vacancy.id)
         return cls.cute_model.rang_candidates(user_info, matched_vacancies_ids)
